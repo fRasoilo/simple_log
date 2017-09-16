b43dc12dc302b327cd28c1e9c4ef5c6a7e1c282e
Filipe Rasoilo <rasoilo@gmail.com>
2017-09-16 22:02:50 +0100

    Continued 'port' of logger until Win32Specific header

--- simple_logger.h
+++ simple_logger.h
@@ -1,3 +1,10 @@
+
+//TODO:
+// - Add the types
+// 
+//
+//
+
 #pragma once
 
 //Simple Logger --------------
@@ -18,72 +25,73 @@
 
 
 // [INTERNAL]  Date and Time --------------
-enum day_of_week
+enum DayOfWeek
 {
-    DayOfWeek_sunday,
-    DayOfWeek_monday,
-    DayOfWeek_tuesday,
-    DayOfWeek_wednesday,
-    DayOfWeek_thursday,
-    DayOfWeek_friday,
-    DayOfWeek_saturday,
+    DayOfWeek_Sunday,
+    DayOfWeek_Monday,
+    DayOfWeek_Tuesday,
+    DayOfWeek_Wednesday,
+    DayOfWeek_Thursday,
+    DayOfWeek_Friday,
+    DayOfWeek_Saturday,
 
-    DayOfWeek_number_of_days
+    DayOfWeek_Count
 };
 
-enum month
+enum Month
 {
-    Month_january   = 1,
-    Month_february  = 2,
-    Month_march     = 3,
-    Month_april     = 4,
-    Month_may       = 5,
-    Month_june      = 6,
-    Month_july      = 7,
-    Month_august    = 8,
-    Month_september = 9,
-    Month_october   = 10,
-    Month_november  = 11,
-    Month_december  = 12,
+    Month_January   = 1,
+    Month_February  = 2,
+    Month_March     = 3,
+    Month_April     = 4,
+    Month_May       = 5,
+    Month_June      = 6,
+    Month_July      = 7,
+    Month_August    = 8,
+    Month_September = 9,
+    Month_October   = 10,
+    Month_November  = 11,
+    Month_December  = 12,
 
-    Month_number_of_months
+    Month_Count
 };
 
-struct date_and_time
+struct DateAndTime
 {
-    month Month;
-    day_of_week DayOfWeek;
-    int32 Day;
-    int32 Hour;
-    int32 Minute;
-    int32 Second;
-    int32 Milliseconds;
+    Month month;
+    DayOfWeek day_of_week;
+    int32 day;
+    int32 hour;
+    int32 minute;
+    int32 second;
+    int32 milliseconds;
 };
 // END Date and Time
 
 // [INTERNAL] Log --------------
-enum log_mode
+enum LogMode
 {
-    LogMode_file               = 0x1,
-    LogMode_dialog             = 0x2,
-    LogMode_file_and_dialog    = 0x3,
-    LogMode_console            = 0x4,
-    LogMode_file_and_console   = 0x5,
-    LogMode_dialog_and_console = 0x6,
-    LogMode_all                = 0x7
+    LogMode_File               = 0x1,
+    LogMode_Dialog             = 0x2,
+    LogMode_FileAndDialog      = 0x3,
+    LogMode_Console            = 0x4,
+    LogMode_FileAndConsole     = 0x5,
+    LogMode_DialogAndConsole   = 0x6,
+    LogMode_All                = 0x7
 };
-enum log_level
+
+enum LogLovel
 {
-    LogLevel_normal,
-    LogLevel_warning,
-    LogLevel_error,
-    LogLevel_fatal,
-    LogLevel_info,
-    LogLevel_debug
+    LogLevel_Normal,
+    LogLevel_Warning,
+    LogLevel_Error,
+    LogLevel_Fatal,
+    LogLevel_Info,
+    LogLevel_Debug
 };
 
 //Forward Declare
-struct log_state;
+struct LogState;
 
 #define PLATFORM_CUSTOM_LOG_TO_FILE(name) bool32 name(log_state* LogState, char* Text)
 typedef PLATFORM_CUSTOM_LOG_TO_FILE(platform_custom_log_to_file);
@@ -98,21 +106,21 @@ typedef PLATFORM_CUSTOM_LOG_TO_CONSOLE(platform_custom_log_to_console);
 typedef PLATFORM_CUSTOM_ERROR_MESSAGE_BOX(platform_custom_error_message_box);
 
 
-struct log_state
+struct LogState
 {
-    bool32 Initialized;
-    log_mode LogMode;
-    log_level LogLevel;
-    char* FilePath;
+    bool32   initialized;
+    LogMode  log_mode;
+    LogLevel log_level;
+    char*    file_path;
 
     //NOTE(filipe): Hooks to the functions that will do the different kinds of
     //logging. Only if we want to add custom stuff at runtime or compile time.
-    platform_custom_log_to_file*   PlatformCustomLogToFile;
-    platform_custom_log_to_window* PlatformCustomLogToWindow;
-    platform_custom_log_to_console* PlatformCustomLogToConsole;
+    platform_custom_log_to_file*    platform_custom_log_to_file;
+    platform_custom_log_to_window*  platform_custom_log_to_window;
+    platform_custom_log_to_console* platform_custom_log_console;
 
     //NOTE(filipe): Hooks to the MessageBox Functions
-    platform_custom_error_message_box* PlatformCustomErrorMessageBox;
+    platform_custom_error_message_box* platform_custom_error_message_box;
     
     //NOTE(filipe): Win32 Specific  
     HANDLE FileHandle;
@@ -126,12 +134,135 @@ struct log_state
 
 // [IMPLEMENTATION]
 
-log_state*
-GetLogState(void)
+LogState*
+sl_logstate_get(void)
 {
-    local_persist log_state LogState = {};
-    return(&LogState);
+    local_persist LogState log_state = {};
+    return(&log_state);
 }
 
 
 
+// Date and Time --------------
+
+//TODO: More formating options for date
+//TODO: Replace, string_buffer from gw_tool
+void sl_date_string_get(DateAndTime date_and_time,string_buffer* StringBuffer)
+{
+    
+    /*
+      Default format for date is  - 1st  January  2015
+                                  - 30th November 2015
+    */
+
+    char* month;
+    switch(date_and_time.month)
+    {
+        case Month_January  :{month = "January ";}break;
+        case Month_February :{month = "February ";}break;
+        case Month_March    :{month = "March ";}break;
+        case Month_April    :{month = "April ";}break;
+        case Month_May      :{month = "May ";}break;
+        case Month_June     :{month = "June ";}break;
+        case Month_July     :{month = "July ";}break;
+        case Month_August   :{month = "August ";}break;
+        case Month_September:{month = "September ";}break;
+        case Month_October  :{month = "October ";}break;
+        case Month_November :{month = "November ";}break;
+        case Month_December :{month = "December ";}break;
+
+            InvalidDefaultCase;
+    }
+    //NOTE: 1 - 31 and \0 -> size 3
+    char day[3];
+    IntToStr(date_and_time.day, day); //TODO: Replace, from gw_tool
+    
+    char* day_post_fix;
+    switch(date_and_time.day)
+    {
+        case 1 :{day_post_fix = "st "; }break;
+        case 21:{day_post_fix = "st "; }break;
+        case 31:{day_post_fix = "st "; }break;
+
+        case 2 :{day_post_fix = "nd "; }break;
+        case 22:{day_post_fix = "nd "; }break;
+        
+        case 3 :{day_post_fix = "rd "; }break;
+        case 23:{day_post_fix = "rd "; }break;
+            
+        default:{day_post_fix = "th ";}
+    }
+
+    //TODO: Replace, from gw_tool
+    append_string(StringBuffer,day);
+    append_string(StringBuffer,day_post_fix);
+    append_string(StringBuffer,month);        
+}
+
+//TODO: Replace, stringbuffer from gw_tool
+void sl_time_string_get(DateAndTime date_and_time, string_buffer* StringBuffer)
+{
+    
+    /*
+      Default format for time is  - HH:MM:SS
+                                  - 10:05:30  -> 10 AM, 5 min, 30 sec
+    */
+    
+    char hour[2]; //0 - 23
+    char minute[2]; // 0 - 60
+    char decond[2]; // 0 - 60
+
+    //TODO: Replace, IntToStr from gw_tool
+    IntToStr(date_and_time.hour,    hour);
+    IntToStr(date_and_time.minute,  minute);
+    IntToStr(date_and_time.second,  second);
+
+    //TODO: Replace, append_string from gw_tool
+    append_string(StringBuffer,hour);
+    append_string(StringBuffer,":");
+    append_string(StringBuffer,minute);
+    append_string(StringBuffer,":");
+    append_string(StringBuffer,second);   
+}
+
+//END Date and Time --------------
+
+//[INTERNAL] Print Color --------------
+
+
+//WIN32
+#if _WIN31
+   #define PrintC(Text,TextColor)  Win32PrintColor(Text, TextColor)
+   #define Print(Text)             write_string_to_console(Text)
+
+#define SL_CONSOLE_FOREGROUND_BLACK     0x0000
+#define SL_CONSOLE_FOREGROUND_BLUE      0x0001
+#define SL_CONSOLE_FOREGROUND_GREEN     0x0002
+#define SL_CONSOLE_FOREGROUND_CYAN      0x0003
+#define SL_CONSOLE_FOREGROUND_RED       0x0004
+#define SL_CONSOLE_FOREGROUND_MAGENTA   0x0005
+#define SL_CONSOLE_FOREGROUND_YELLOW    0x0006
+#define SL_CONSOLE_FOREGROUND_GREY      0x0007
+#define SL_CONSOLE_FOREGROUND_INTENSITY 0x0008 //foreground color is intensified.
+
+#define SL_CONSOLE_BACKGROUND_BLACK     0x0000
+#define SL_CONSOLE_BACKGROUND_BLUE      0x0010
+#define SL_CONSOLE_BACKGROUND_GREEN     0x0020
+#define SL_CONSOLE_BACKGROUND_CYAN      0x0030
+#define SL_CONSOLE_BACKGROUND_RED       0x0040
+#define SL_CONSOLE_BACKGROUND_MAGENTA   0x0050
+#define SL_CONSOLE_BACKGROUND_YELLOW    0x0060
+#define SL_CONSOLE_BACKGROUND_GREY      0x0070
+#define SL_CONSOLE_BACKGROUND_INTENSITY 0x0080 // background color is intensified.
+
+#define EXTRACT_BG(Value) (Value & 0xF0)
+#define EXTRACT_FG(Value) (Value & 0x0F)
+
+#else
+   #error No other OS defined!
+#endif //END WIN32
+//TODO: Other OS
+
+
+
+//END Print Color --------------
